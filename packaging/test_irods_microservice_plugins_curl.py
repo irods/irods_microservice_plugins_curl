00d961f91b5462b2fc919a328fd419ee7db4299f
Ben Keller <irods.ben.keller@gmail.com>
2016-02-02 14:51:38 -0500

    [#7] Update tests for 4.1 framework

--- packaging/test_irods_microservice_plugins_curl.py
+++ packaging/test_irods_microservice_plugins_curl.py
@@ -1,37 +1,34 @@
+import os
 import sys
-if (sys.version_info >= (2,7)):
+if sys.version_info >= (2,7):
     import unittest
 else:
     import unittest2 as unittest
-import pydevtest_common as c
-import pydevtest_sessions as s
 
-
-class Test_MS_Plugin_CURL(unittest.TestCase):
-    rules_dir = c.get_irods_top_level_dir() + "/iRODS/clients/icommands/test/rules3.0/"
-    dest_obj="/tempZone/home/public/ferrari.art" # will need to be passed to curlGetObj.r
+import lib
     
-    def setUp(self):
-        s.admin_up()
-        
+
+SessionsMixin = lib.make_sessions_mixin([('otherrods', 'apass')], [])
+
+class Test_MS_Plugin_CURL(SessionsMixin, unittest.TestCase):
+    rules_dir = os.path.join(lib.get_irods_top_level_dir(), 'iRODS', 'clients', 'icommands', 'test', 'rules3.0')
+    dest_obj = '/tempZone/home/public/ferrari.art' # will need to be passed to curlGetObj.r
+
     def tearDown(self):
-        s.adminsession.runCmd('irm', ['-f', self.dest_obj])
-        s.admin_down()
-        
+        self.admin_sessions[0].run_icommand(['irm', '-f', self.dest_obj])
+        super(Test_MS_Plugin_CURL, self).tearDown()
+    
     def test_curl_get_obj(self):
-        rule_file = self.rules_dir + "curlGetObj.r"
-        print "-- running "+rule_file
-        c.assertiCmd(s.adminsession,"irule -vF "+rule_file, "LIST", "completed successfully")
+        rule_file = os.path.join(self.rules_dir, 'curlGetObj.r')
+        self.admin_sessions[0].assert_icommand(['irule', '-vF', rule_file], 'STDOUT_SINGLELINE', 'completed successfully')
         
     def test_curl_get_str(self):
-        rule_file = self.rules_dir + "curlGetStr.r"
-        print "-- running "+rule_file
-        c.assertiCmd(s.adminsession,"irule -vF "+rule_file, "LIST", "completed successfully")
+        rule_file = os.path.join(self.rules_dir, 'curlGetStr.r')
+        self.admin_sessions[0].assert_icommand(['irule', '-vF', rule_file], 'STDOUT_SINGLELINE', "completed successfully")
         
     def test_curl_post(self):
-        rule_file = self.rules_dir + "curlPost.r"
-        print "-- running "+rule_file
+        rule_file = os.path.join(self.rules_dir, 'curlPost.r')
         
         # will have to dynamically pass form_data to the rule once that's fixed
-        form_data = "Sent from iRODS"
-        c.assertiCmd(s.adminsession,"irule -F "+rule_file, "LIST", form_data)
+        form_data = 'Sent from iRODS'
+        self.admin_sessions[0].assert_icommand(['irule', '-F', rule_file], 'STDOUT_SINGLELINE', form_data)
