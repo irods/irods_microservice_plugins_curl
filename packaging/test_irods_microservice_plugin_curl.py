40d5c5322894572c3229a207f6f4453e9bff0ab0
Ben Keller <irods.ben.keller@gmail.com>
2016-02-03 11:35:12 -0500

    [#7] Rename python test file
    plugin is singular in all other plugin test file names

--- packaging/test_irods_microservice_plugin_curl.py
+++ packaging/test_irods_microservice_plugin_curl.py
@@ -6,7 +6,7 @@ else:
     import unittest2 as unittest
 
 import lib
-    
+
 
 SessionsMixin = lib.make_sessions_mixin([('otherrods', 'apass')], [])
 
@@ -17,18 +17,18 @@ class Test_MS_Plugin_CURL(SessionsMixin, unittest.TestCase):
     def tearDown(self):
         self.admin_sessions[0].run_icommand(['irm', '-f', self.dest_obj])
         super(Test_MS_Plugin_CURL, self).tearDown()
-    
+
     def test_curl_get_obj(self):
         rule_file = os.path.join(self.rules_dir, 'curlGetObj.r')
         self.admin_sessions[0].assert_icommand(['irule', '-vF', rule_file], 'STDOUT_SINGLELINE', 'completed successfully')
-        
+
     def test_curl_get_str(self):
         rule_file = os.path.join(self.rules_dir, 'curlGetStr.r')
         self.admin_sessions[0].assert_icommand(['irule', '-vF', rule_file], 'STDOUT_SINGLELINE', "completed successfully")
-        
+
     def test_curl_post(self):
         rule_file = os.path.join(self.rules_dir, 'curlPost.r')
-        
+
         # will have to dynamically pass form_data to the rule once that's fixed
         form_data = 'Sent from iRODS'
         self.admin_sessions[0].assert_icommand(['irule', '-F', rule_file], 'STDOUT_SINGLELINE', form_data)
