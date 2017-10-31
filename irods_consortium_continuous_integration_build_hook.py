9f4a368abd81d5bd0258aaa3a79a9f82c7642fbc
Terrell Russell <terrellrussell@gmail.com>
2017-10-31 11:39:02 -0400

    add gcc to apt build prerequisites

--- irods_consortium_continuous_integration_build_hook.py
+++ irods_consortium_continuous_integration_build_hook.py
@@ -24,7 +24,7 @@ def get_build_prerequisites_all():
             'irods-externals-zeromq4-14.1.3-0']
 
 def get_build_prerequisites_apt():
-    return ['libcurl4-gnutls-dev', 'make', 'libssl-dev'] + get_build_prerequisites_all()
+    return ['libcurl4-gnutls-dev', 'make', 'libssl-dev', 'gcc'] + get_build_prerequisites_all()
 
 def get_build_prerequisites_yum():
     return ['curl-devel', 'openssl-devel'] + get_build_prerequisites_all()
