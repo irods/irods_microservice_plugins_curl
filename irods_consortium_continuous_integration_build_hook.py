ec0c615e7128399a45e23c057942dbb48ab04775
Kory Draughn <korydraughn@renci.org>
2023-10-11 09:22:03 -0400

    [irods/irods#7247] Update dependencies in build hook.

--- irods_consortium_continuous_integration_build_hook.py
+++ irods_consortium_continuous_integration_build_hook.py
@@ -16,8 +16,8 @@ def add_cmake_to_front_of_path():
 
 def install_building_dependencies(externals_directory):
     externals_list = [
-        'irods-externals-avro1.11.0-1',
-        'irods-externals-boost1.78.0-0',
+        'irods-externals-avro1.11.0-2',
+        'irods-externals-boost1.81.0-0',
         'irods-externals-clang-runtime13.0.0-0',
         'irods-externals-clang13.0.0-0',
         'irods-externals-cmake3.21.4-0',
